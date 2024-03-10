ce2150b0fad79a86f96e7386c7dea31bacf33686
Vivek <www.vivekeduchigure@gmail.com>
2024-03-10 20:21:19 +0530

    All components are added

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const authController = require("./controllers/authController");
+const productController = require("./controllers/productController");
+const uploadController = require("./controllers/uploadController");
 const app = express();
 
 // connect our db
@@ -10,9 +12,12 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, console.log("DB is connected"));
 
 // rotes & middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authController);
+app.use("/product", productController);
+app.use("/upload", uploadController);
 
 // strart our server
 app.listen(process.env.PORT, () =>
